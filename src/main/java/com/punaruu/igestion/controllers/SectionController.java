d1cf44eba8c34e87d244aa996faa30f9c14d6a9a
teataHeiava <heiava.teata@informatique.gov.pf>
2020-03-23 22:03:10 -1000

    controller get section

--- src/main/java/com/punaruu/igestion/controllers/SectionController.java
+++ src/main/java/com/punaruu/igestion/controllers/SectionController.java
@@ -1,14 +1,12 @@
 package com.punaruu.igestion.controllers;
 
 import com.punaruu.igestion.domain.entities.Section;
+import com.punaruu.igestion.domain.enumerations.DisciplineEnum;
 import com.punaruu.igestion.services.SectionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,4 +21,11 @@ public class SectionController {
     ResponseEntity<List<Section>> getAll() {
         return new ResponseEntity<>(this.sectionService.listerTous(), HttpStatus.OK);
     }
+
+    @RequestMapping(value = "/api/v1/sections/{discipline}", produces = { "application/json" }, method = RequestMethod.GET)
+    ResponseEntity<Section> getOne(@PathVariable("discipline") DisciplineEnum discipline) {
+        return new ResponseEntity<>(this.sectionService.trouverParDiscipline(discipline), HttpStatus.OK);
+    }
+
+
 }
